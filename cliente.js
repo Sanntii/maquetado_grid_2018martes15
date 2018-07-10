39a5629c5369a2b4ee40451991f95b5f4ca12d2e
Glitch (maquetadogrid2018martes) <none>
2018-07-10 20:17:08 +0000

    🐵🏇 Updated with Glitch

---
+++ cliente.js
@@ -0,0 +1,32 @@
+
+
+
+class Distribuidora {
+  
+  static ofertar (){
+    console.log('[..] Programando en Javascript en Buenos Aires');
+    
+
+    //document.querySelector("#panelMsg").innerHTML += "Algo!" ;
+    
+    
+    document.querySelector("#panelMsg").innerHTML += document.querySelector("#golosina_titulo").value 
+    + document.querySelector("#golosina_descripcion").value ;
+    
+    
+    
+    
+    
+    //  alert(1123456789);
+    
+    console.log('[..] Programando en Javascript en Buenos Aires');
+    
+  }
+  
+  
+  
+}
+
+
+
+
